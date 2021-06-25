48d403fc686a25e42a5e917c2d8444c4af482b87
Mohanad-Elagan <81828865+Mohanad-Elagan@users.noreply.github.com>
2021-06-25 18:01:29 +0200

    Add files via upload

---
+++ solution.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Solution</title>
+    <link rel="shortcut icon" href="pics\solution_1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_about.css">
+    <link rel="stylesheet" href="style_article.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+
+<body class="solution fade-in">
+    <nav role='navigation'>
+        <a data-page="home" class="" href="index.php">Home</a>
+        <a data-page="solution" class="active" href="solution.php">USA</a>
+        <a data-page="survey" class="" href="survey.php">Asia</a>
+        <a data-page="about" class="" href="about.php">Europe</a>
+        <a data-page="about" class="" href="about.php">About</a>
+    </nav>
+
+    <div class="title_div" style="height: fit-content;">
+        <img src="pics\Logo.png" alt="Revilky">
+        <h1 style="display: inline-block;">USA</h1>
+    </div>
+
+    <article class="solution">
+        <p class="tag">According to the data collected by EO Dashboard, the COVID – 19 pandemics have had an impact on the density of autos in international airports in the United States. The number of automobiles in airports has decreased significantly after the COVID-19 pandemics, as shown in <strong><em>(figure 1)</em></strong>; nevertheless, this significant reduction began in March 2020 and has continued unabated since then. Since the lockdown due to the COVID-19, the traffic has vanished as the density of cars has decreased. This reduction in car density in the airport's by-parking areas substantially impacts two aspects: economics and air quality.</p>
+        <div class="figure">
+        <img src="pics\Figure 1.png" alt="figure 1">
+        </div>
+        <br><br>
+        <h3 class="margin_left">Economic impact: </h3>
+        <br>
+        <p class="tag">The decrease in automobiles has had a significant impact on the economy. Due to the COVID–19 crisis, more than one billion passengers were removed, representing a decline of 64.4% of global passenger traffic because of the travel limitations. Due to the drop in demand for automobiles, the vehicle service businesses have chosen to dismiss their employees to cut costs as much as feasible. In comparison to the number of workers in 2019, 10.05 % of workers have lost their employment, putting their families in poverty. Furthermore, the sector has been impacted since the total sales during the second quarter in 2020 declined by about 34%. This industry not only has an influence on itself, but it also has an impact on the other industries; the U.S. Department of transportation loses 43.5$ million a day in federal gas tax losses. The economic effect of this business has had a wide range of consequences that have harmed the community.</p>
+        <br><br>
+        <h3 class="margin_left">Air quality impact: </h3>
+        <br>
+        <p class="tag">Automobiles have a significant influence on air quality in general. The combustion of fossil fuels produces a large number of greenhouse gases, which has a significant impact on air quality; however, due to the COVID-19 pandemics, the amount of CO 2 generated has shown a 17% decrease in daily global CO 2 emission by early April 2020 compared to the mean level in 2019 as shown in figure 2. Following the global lockdown, there was a massive fall in NO2 
+about 45% to 50%. In urban areas in North America, NO2 levels were lowered by 19–28%. This was mainly due to a reduction in traffic emissions in urban areas. Furthermore, the Ozone Hole has been closed for the first time since 1980; yet, the industry has suffered huge losses; conversely, air quality has improved. Figure 3 shows the difference between how polluted the air quality was in 2015-2019 and how it is after the COVID-19 (after March 2020).</p>
+        <img src="pics\Figure 2.png" alt="figure 2">
+        <img src="pics\Figure 3.png" alt="figure 3">
+    </article>
+    
+    <script>
+        window.scrollBy(0, 1);
+        window.scrollBy(0, -1);
+    </script>
+</body>
+
+</html>
