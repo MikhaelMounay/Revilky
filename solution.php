015b7c038f0d239c955cc7ae832956bb6d307e72
Mohanad-Elagan <81828865+Mohanad-Elagan@users.noreply.github.com>
2021-06-27 01:25:47 +0200

    Add files via upload

--- solution.php
+++ solution.php
@@ -29,9 +29,10 @@
 
     <article class="solution">
         <p class="tag">According to the data collected by EO Dashboard, the COVID – 19 pandemics have had an impact on the density of autos in international airports in the United States. The number of automobiles in airports has decreased significantly after the COVID-19 pandemics, as shown in <strong><em>(figure 1)</em></strong>; nevertheless, this significant reduction began in March 2020 and has continued unabated since then. Since the lockdown due to the COVID-19, the traffic has vanished as the density of cars has decreased. This reduction in car density in the airport's by-parking areas substantially impacts two aspects: economics and air quality.</p>
-        <div class="figure">
-        <img src="pics\Figure 1.png" alt="figure 1">
-        </div>
+        <figure>
+        <img src="pics\Figure 1.png" alt="figure 1" style="width:100%">
+        <figcaption>Figure 1; Car Density in different times</figcaption>
+        </figure>
         <br><br>
         <h3 class="margin_left">Economic impact: </h3>
         <br>
@@ -41,8 +42,14 @@
         <br>
         <p class="tag">Automobiles have a significant influence on air quality in general. The combustion of fossil fuels produces a large number of greenhouse gases, which has a significant impact on air quality; however, due to the COVID-19 pandemics, the amount of CO 2 generated has shown a 17% decrease in daily global CO 2 emission by early April 2020 compared to the mean level in 2019 as shown in figure 2. Following the global lockdown, there was a massive fall in NO2 
 about 45% to 50%. In urban areas in North America, NO2 levels were lowered by 19–28%. This was mainly due to a reduction in traffic emissions in urban areas. Furthermore, the Ozone Hole has been closed for the first time since 1980; yet, the industry has suffered huge losses; conversely, air quality has improved. Figure 3 shows the difference between how polluted the air quality was in 2015-2019 and how it is after the COVID-19 (after March 2020).</p>
+        <figure> 
         <img src="pics\Figure 2.png" alt="figure 2">
+        <figcaption>Figure 2; amount of NO2 in different times.</figcaption>
+        </figure>
+        <figure>
         <img src="pics\Figure 3.png" alt="figure 3">
+        <figcaption>Figure 3; difference between air quality in different times.</figcaption>
+        </figure>
     </article>
     
     <script>
